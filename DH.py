ca97e21832cab9584254aee0e8076ce12f68677f
RagnarOKay <workBelkanov@gmail.com>
2021-11-22 17:34:18 +0300

    DaDa

--- DH.py
+++ DH.py
@@ -1,3 +1,4 @@
+import random
 
 class DH():
     def __init__(self, public_key1, public_key2, private_key):
@@ -29,32 +30,66 @@ class DH():
             decrypted_message += chr(ord(c) - key)
         return decrypted_message
 
+def generate_primes(n):
+    primes = [2]
+    nextPrime = 3
+    while nextPrime < n:
+        isPrime = True
+        i = 0
+        squareRoot = int(nextPrime ** 0.5)
+        while primes[i] <= squareRoot:
+            if nextPrime % primes[i] == 0:
+                isPrime = False
+            i += 1
+        if isPrime:
+            primes.append(nextPrime)
+
+        nextPrime += 1
+    return primes
+
+def get_prime():
+
+    while True:
+        num = random.randint(2, 1000)
+        if num in generate_primes(1000):
+            return num
+
+
+
 message = 'Berlin is the capital of great Germany '
-alice_public = 197
-alice_private = 199
-bob_public = 151
-bob_private = 157
+
+alice_public = get_prime()
+alice_private = get_prime()
+bob_public = get_prime()
+bob_private = get_prime()
+eva_private = get_prime()
+
 alice = DH(alice_public, bob_public, alice_private)
 bob = DH(alice_public, bob_public, bob_private)
+eva = DH(alice_public, bob_public, eva_private)
 
+
+alice_partial = alice.generate_partial_key()
+bob_partial = bob.generate_partial_key()
+eva_partial = eva.generate_partial_key()
+alice_full = alice.generate_full_key(bob_partial)
+bob_full = bob.generate_full_key(alice_partial)
+eva_full = eva.generate_full_key(alice_partial)
+
+bob_encrypted = bob.encrypt_message(message)
+dec_message = alice.decrypt_message(bob_encrypted)
 
 
 if __name__ == "__main__":
-    s_partial = alice.generate_partial_key()
-    print(s_partial)  # 147
+  print(f'алиса\n* '
+        f'публичный: {str(alice_public)}\n* личный:  {str(alice_private)}\n* '
+        f'частичный: {str(alice_partial)}\n* полный: {str(alice_full)}')
+  print(f'боб\n* '
+        f'публичный: {str(bob_public)}\n* личный:  {str(bob_private)}\n*'
+        f' частичный: {str(bob_partial)}\n* полный: {str(bob_full)}')
+  print(f'ева\n* '
+        f'личный: {str(eva_private)}\n* частичный:  {str(eva_partial)}\n* '
+        f'полный: {str(eva_full)}')
 
-    m_partial = bob.generate_partial_key()
-    print(m_partial)  # 66
-
-    s_full = alice.generate_full_key(m_partial)
-    print(s_full)  # 75
-
-    m_full = bob.generate_full_key(s_partial)
-    print(m_full)  # 75
-
-    m_encrypted = bob.encrypt_message(message)
-    print(m_encrypted)
-
-    message_final = alice.decrypt_message(m_encrypted)
-    print(message_final)
-
+  print('Зашифрованное сообщение: ' + bob_encrypted)
+  print("Расшифрованное сообщение: " + dec_message)
